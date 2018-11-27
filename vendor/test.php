d9d33313714f31f34b20c6c1755ad1b3cf36380b
Fanny Pierre <fanny.pierre1@etu.univ-lorraine.fr>
2018-11-27 11:11:22 +0100

    complétion de la classe test.php

--- vendor/test.php
+++ vendor/test.php
@@ -15,10 +15,26 @@ $db->addConnection(parse_ini_file('src/conf/conf.ini'));
 $db->setAsGlobal();
 $db->bootEloquent();
 
+//Mes listes
 $listes = m\Liste::get();
-foreach ($listes as $liste) echo '<br>' . $liste->titre;
+foreach ($listes as $liste) echo '<br>' . $liste->titre . '(' . $liste->no . ')';
 
 echo '<br><br>';
 
+//Mes items
 $items = m\Item::get();
 foreach ($items as $item) echo '<br>' . $item->nom;
+
+//Item dont l'id est passé en parmaetre
+$id = 1;
+if ( isset($_GET['id']) ) {
+    if (!is_null( $_GET['id'])){
+        $id = $_GET['id'];
+    }
+} //? $_GET('id') : 1;
+
+$MyItem = m\Item::where('id', '=', $id)->first();
+
+var_dump($_GET['id']);
+
+echo "Votre item ($id) : " . $MyItem->nom;
