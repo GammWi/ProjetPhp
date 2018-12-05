6a593d2b106c7889b745eb8eb278c6f2a35530bb
Emile <emile.rey3@etu.univ-lorraine.fr>
2018-12-05 11:09:36 +0100

    Création de la méthode afficherListe

---
+++ methodes/afficherListe.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emile
+ * Date: 05/12/2018
+ * Time: 11:04
+ */
+
+require_once '../vendor/autoload.php';
+
+use wishlist\models as m;
+
+$db = new Illuminate\Database\Capsule\Manager();
+$db->addConnection(parse_ini_file('../src/conf/conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+echo('Utiliser : afficherListe.php?liste=ID' . '<br>');
+
+$listeid = -1;
+if ( isset($_GET['liste']) ) {
+    if (!is_null( $_GET['liste'])){
+        $listeid = $_GET['liste'];
+    }
+}
+if($listeid != -1){
+    $liste = m\Liste::where('no', '=', $listeid)->first();
+    foreach($liste->items as $item){
+        echo '</br> Item : ' . $item->nom ;
+    }
+}
