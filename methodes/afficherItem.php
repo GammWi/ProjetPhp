62006267b32d4bc9e5e6b3de9c32c5ded328a801
Emile <emile.rey3@etu.univ-lorraine.fr>
2018-12-05 11:24:07 +0100

    Ajout des images

---
+++ methodes/afficherItem.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emile
+ * Date: 05/12/2018
+ * Time: 11:19
+ */
+
+require_once '../vendor/autoload.php';
+
+use wishlist\models as m;
+
+$db = new Illuminate\Database\Capsule\Manager();
+$db->addConnection(parse_ini_file('../src/conf/conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+$itemid = -1;
+if ( isset($_GET['item']) ) {
+    if (!is_null( $_GET['item'])){
+        $itemid = $_GET['item'];
+    }
+}
+if($itemid != -1){
+    $item = m\Item::where('id', '=', $itemid)->first();
+    echo($item->nom . ' (' . $item->descr . ') : ' . $item->tarif . '€');
+    echo('<br><img src="../img/'. $item->img . '"height="64">');
+} else {
+    echo('Utiliser : afficherItem.php?item=ID' . '<br>');
+}
--- methodes/afficherListe.php
+++ methodes/afficherListe.php
@@ -15,8 +15,6 @@ $db->addConnection(parse_ini_file('../src/conf/conf.ini'));
 $db->setAsGlobal();
 $db->bootEloquent();
 
-echo('Utiliser : afficherListe.php?liste=ID' . '<br>');
-
 $listeid = -1;
 if ( isset($_GET['liste']) ) {
     if (!is_null( $_GET['liste'])){
@@ -25,7 +23,11 @@ if ( isset($_GET['liste']) ) {
 }
 if($listeid != -1){
     $liste = m\Liste::where('no', '=', $listeid)->first();
+    echo('Items de la liste : "' . $liste->titre . '"<br>');
     foreach($liste->items as $item){
-        echo '</br> Item : ' . $item->nom ;
+        echo('</br> - ' . $item->nom . ' (' . $item->descr . ') : ' . $item->tarif . '€');
+        echo('<br><img src="../img/'. $item->img . '"height="64">');
     }
+} else {
+    echo('Utiliser : afficherListe.php?liste=ID' . '<br>');
 }
