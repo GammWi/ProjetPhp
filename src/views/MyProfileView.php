5d79ef55f7d5194ea946c99acfcb0a327f420804
Emile <emile.rey3@etu.univ-lorraine.fr>
2018-12-27 13:52:24 +0100

    Update profile

--- src/views/MyProfileView.php
+++ src/views/MyProfileView.php
@@ -43,7 +43,7 @@ class MyProfileView extends AbstractView
 
               <ul class="list-group list-group-unbordered">
                 <li class="list-group-item">
-                  <b>Listes</b> <a class="pull-right">-1</a>
+                  <b>Listes</b> <a class="pull-right">{$this->u->listes->count()}</a>
                 </li>
                 <li class="list-group-item">
                   <b>Amis</b> <a class="pull-right">0</a>
