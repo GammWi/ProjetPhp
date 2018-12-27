c97cf7e4c8e9a4eb5b426e24d1ba7d5fdfc962bf
Emile <emile.rey3@etu.univ-lorraine.fr>
2018-12-27 14:03:16 +0100

    Intégration de la navbar à AbstractView

--- src/views/AbstractView.php
+++ src/views/AbstractView.php
@@ -70,10 +70,56 @@ END;
         $html = <<<END
 <body class="hold-transition skin-red sidebar-mini">
 <div class="wrapper">
-END;
 
-    include("navbar.php");
-    $html.=<<<END
+<header class="main-header">
+    <!-- Logo -->
+    <a href="/" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>M</b>WL</span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>My</b>WishList</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+    </nav>
+</header>
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="/web/profile/default.jpg" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+END;
+        if(!empty($_SESSION)){
+            $html .= "<p>" . m\User::where('id', '=', $_SESSION['id'])->first()->name . "</p>";
+        } else {
+            $html .= "<p>" . m\User::where('id', '=', $_SESSION['id'])->first()->name . "</p>";
+        }
+        $html .= <<<END
+            <a href="/Deconnexion.php"><i class="fa fa-circle text-success"></i> Se déconnecter</a>
+            </div>
+        </div>
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">Navigation</li>
+            <li><a href="/index.php/listes"><i class="fa fa-list"></i> <span>Toutes les listes</span></a></li>
+            <li><a href="/index.php/listeMembres"><i class="fa fa-users"></i> <span>Membres</span></a></li>
+            <li class="header">Espace personnel</li>
+            <li><a href="/index.php/afficherMyProfile"><i class="fa fa-user"></i> <span>Mon compte</span></a></li>
+            <li><a href="/index.php/createListe"><i class="fa fa-plus"></i> <span>Créer une liste</span></a></li>
+            <li><a href="/index.php/myListes"><i class="fa fa-list"></i> <span>Mes listes</span></a></li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
