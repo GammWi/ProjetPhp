6e3770ce52b8304ceb2fcb736078597d2ef49395
Emile <emile.rey3@etu.univ-lorraine.fr>
2019-01-09 11:20:28 +0100

    Suppression modal inutile

--- src/views/CreateListeView.php
+++ src/views/CreateListeView.php
@@ -93,47 +93,6 @@ END;
                 </div>
             </div>
         </div>
-
-        <!-- modal pour associer une liste -->
-        <div class="modal fade" id="modal-associer-liste">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Associer une liste existante à mon compte</h4>
-              </div>
-              <div class="modal-body">
-END;
-        if(isset($_SESSION['id'])){
-            $html .= <<<END
-                <form action="$nouveauMessage" method="post" class="form-horizontal">
-                    <div class="box-body">
-                        <div class="form-group">
-                          <label>Token de la liste</label>
-                          <input type="text" class="form-control" name="message" placeholder="xxxxxxxxxxxxx">
-                        </div>
-                        <input type="hidden" name="user_id" value="{$_SESSION['id']}"/>
-                    </div>
-                    <div class="box-footer">
-                        <button type="submit" class="btn btn-danger pull-right">Associer la liste à mon compte</button>
-                    </div>
-                </form>
-END;
-        } else {
-            $html .= <<<END
-                        <div class="callout callout-danger">
-                            <h4>Erreur !</h4>
-
-                            <p>Vous devez être connecté à un compte utilisateur pour associer une liste.</p>
-                        </div>
-END;
-        }
-        $html .= <<<END
-              </div>
-            </div>
-          </div>
-        </div>
 END;
         echo $html;
     }
